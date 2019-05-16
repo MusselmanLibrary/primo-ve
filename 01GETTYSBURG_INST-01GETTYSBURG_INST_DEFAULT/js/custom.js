573888d7fad7b5712e25c2b507d65c52a468bbf9
Musselman Library <50455321+MusselmanLibrary@users.noreply.github.com>
2019-05-16 09:42:18 -0400

    added link to worldcat

--- 01GETTYSBURG_INST-01GETTYSBURG_INST_DEFAULT/js/custom.js
+++ 01GETTYSBURG_INST-01GETTYSBURG_INST_DEFAULT/js/custom.js
@@ -16,4 +16,19 @@
 
 })
 
+app.component('searchWorldCat', {
+    
+});
+
+app.component('prmSearchBarAfter', {
+
+});
+
+app.component('searchWorldCat', {
+    template: `<span style="margin-left: 10%;"><a href="http://ezpro.cc.gettysburg.edu:2048/login?url=http://firstsearch.oclc.org/fsip?dbname=worldcat&done=referer">Search other libraries</a></span>`
+});
    
+app.component('prmSearchBarAfter', {
+        bindings: {parentCtrl: `<`},
+        template: `<search-world-cat></search-world-cat>`    
+});
