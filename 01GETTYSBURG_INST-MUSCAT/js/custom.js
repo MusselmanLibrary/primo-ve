af40170020d4a66ee02456585860b83874dd79a7
Musselman Library <50455321+MusselmanLibrary@users.noreply.github.com>
2019-08-30 13:14:04 -0400

    updated to add browzine code

--- 01GETTYSBURG_INST-MUSCAT/js/custom.js
+++ 01GETTYSBURG_INST-MUSCAT/js/custom.js
@@ -15,6 +15,39 @@
 
 /***************************************************************/
 
+// Begin BrowZine - Primo Integration...
+  window.browzine = {
+    api: "https://public-api.thirdiron.com/public/v1/libraries/ID",
+    apiKey: "API",
+ 
+    journalCoverImagesEnabled: true,
+ 
+    journalBrowZineWebLinkTextEnabled: true,
+    journalBrowZineWebLinkText: "View Journal Contents",
+ 
+    articleBrowZineWebLinkTextEnabled: true,
+    articleBrowZineWebLinkText: "View Issue Contents",
+ 
+    articlePDFDownloadLinkEnabled: true,
+    articlePDFDownloadLinkText: "Download PDF",
+ 
+    printRecordsIntegrationEnabled: true,
+  };
+ 
+  browzine.script = document.createElement("script");
+  browzine.script.src = "https://s3.amazonaws.com/browzine-adapters/primo/browzine-primo-adapter.js";
+  document.head.appendChild(browzine.script);
+ 
+  app.controller('prmSearchResultAvailabilityLineAfterController', function($scope) {
+    window.browzine.primo.searchResult($scope);
+  });
+ 
+  app.component('prmSearchResultAvailabilityLineAfter', {
+    bindings: { parentCtrl: '<' },
+    controller: 'prmSearchResultAvailabilityLineAfterController'
+  });
+// ... End BrowZine - Primo Integration
+
 // Report a Problem
 
 app.controller('prmActionContainerAfterController', [function () {
@@ -25,7 +58,7 @@ app.controller('prmActionContainerAfterController', [function () {
     function getPermalink() {
         var permalink = encodeURIComponent(window.location.href);
 
-        var formField = 'https://docs.google.com/forms/d/e/1FAIpQLSf-RoeHogXcnLugk8oi_k8YKykv0Q0Ur5ZWO3y95_hptmCALQ/viewform?usp=pp_url&entry.1336545636=';
+        var formField = 'https://docs.google.com/forms/...';
         formField += permalink;
         return formField;
         //return vm.parentCtrl.hasSearchResults;
@@ -39,7 +72,7 @@ app.component('prmActionContainerAfter', {
     template: '<div id="report-problem" layout="row" layout-align="center center"><a id="problemLink" target="_blank" href="{{$ctrl.getPermalink()}}" title="Report a problem"><img src="https://musselmanlibrary.org/primo/images/warning.png">&nbsp;&nbsp;Report a problem with this item</a></div>'
 });
 
-// WorldCat search link
+// WorldCat search link - taken from Williams College
 app.controller('SearchBarAfterController', [function () {
     var vm = this;
 
@@ -52,25 +85,21 @@ app.controller('SearchBarAfterController', [function () {
     vm.getLink = getLink;
 
     function getLink() {
-        //if (vm.parentCtrl.hasSearchResults !=='false') {
         var query = vm.parentCtrl.mainSearchField;
-        return 'http://ezpro.cc.gettysburg.edu:2048/login?url=http://firstsearch.oclc.org/fsip?dbname=worldcat&done=referer&query=' + query;
-        //} else {
-        // return 'http://ezpro.cc.gettysburg.edu:2048/login?url=http://firstsearch.oclc.org/fsip?dbname=worldcat&done=referer';
-        //}
+        return 'your.ezproxy.edu/login?url=http://firstsearch.oclc.org/fsip?dbname=worldcat&done=referer&query=' + query;
     }
 }]);
 
 app.component('prmSearchBarAfter', {
     bindings: { parentCtrl: '<' },
     controller: 'SearchBarAfterController',
-    template: '<div id="worldcatSearchBar-gtxs" layout-row div="" layout="row"><div flex="0" flex-xs="5" flex-sm="0" flex-md="0" flex-lg="15" flex-xl="20"></div><div flex-lg="17" flex-xl="17"><a style="font-color: white;" id="worldcatLink" href="{{$ctrl.getLink()}}" title="Search WorldCat">&nbsp;SEARCH&nbsp;&nbsp;<img ng-src="https://musselmanlibrary.org/primo/images/WorldCat.png" width="22" height="22" alt="WorldCat logo" id="worldcatLogo">&nbsp;&nbsp;OTHER LIBRARIES</a></div></div>'
+    template: '<div id="worldcatSearchBar-gtxs" layout-row div="" layout="row"><div flex="0" flex-xs="5" flex-sm="0" flex-md="0" flex-lg="15" flex-xl="20"></div><div flex-lg="17" flex-xl="17"><a style="font-color: white;" id="worldcatLink" href="{{$ctrl.getLink()}}" title="Search WorldCat">&nbsp;SEARCH&nbsp;&nbsp;<img ng-src="path/to/WorldCat.png" width="22" height="22" alt="WorldCat logo" id="worldcatLogo">&nbsp;&nbsp;OTHER LIBRARIES</a></div></div>'
 });
 
 // Adds the chat button
  (function() {
     var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = 'true';
-    lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'v2.libanswers.com/load_chat.php?hash=bc6e55d2e4d6269040cbfba54b62eb67';
+    lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'v2.libanswers.com/load_chat.php?hash=HASH';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
                 })();
 // End the chat button
